f700151bd040a33cab6eeb83a0f6faa97c4db91b
LaucoTec <a01783129@tec.mx>
2026-07-25 14:21:42 -0600

    Stage 4: Full CRUD

--- main.py
+++ main.py
@@ -56,6 +56,29 @@ async def create_task(task: dict):
     return task
 
 
+@app.put("/tasks/{task_id}")
+async def update_task(task_id: int, updated_task: dict):
+    task = examples.get(task_id)
+    if task is None:
+        raise HTTPException(status_code=404, detail=f"Task with ID {task_id} not found")
+    if not any(key in updated_task for key in ["title", "description", "completed"]):
+        raise HTTPException(
+            status_code=400,
+            detail="Updated task must have at least one of the following fields: title, description, completed",
+        )
+
+    task.update(updated_task)
+    examples[task_id] = task
+    return task
+
+
+@app.delete("/tasks/{task_id}", status_code=204)
+async def delete_task(task_id: int):
+    task = examples.pop(task_id, None)
+    if task is None:
+        raise HTTPException(status_code=404, detail=f"Task with ID {task_id} not found")
+
+
 @app.get("/")
 async def root():
     return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]}
